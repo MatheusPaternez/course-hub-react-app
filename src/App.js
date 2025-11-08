63fa738e1092b3b691c535be362d640b0a0395d3
kent2664 <onz0shi2664@gmail.com>
2025-11-07 23:27:29 -0800

    Add Course-Search basic layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPopup from './pages/LoginPopup';
+import CourseSearch from './pages/CourseSearch';
 
 export default function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/'>
-                    <Route index element={<LoginPopup />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        // <BrowserRouter>
+        //     <Routes>
+        //         <Route path='/'>
+        //             <Route index element={<LoginPopup />} />
+        //         </Route>
+        //     </Routes>
+        // </BrowserRouter>
+        <CourseSearch></CourseSearch>
     )
 }
