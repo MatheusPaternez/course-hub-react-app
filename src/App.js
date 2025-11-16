288da412bc37d8bac3c83fc5c297dff7193a1198
Tiana <tiana.kim7691@gmail.com>
2025-11-15 16:34:11 -0800

    Fixed the issue where the course page and homepage were not rendering properly.

--- src/App.js
+++ src/App.js
@@ -12,38 +12,21 @@ import DashboardStudent from './pages/DashboardStudent';
 export default function App() {
     return (
         <>
-<<<<<<< HEAD
-        <BrowserRouter>
-        <Header />
-            <Routes>
-                <Route path='/'>
-                    <Route element={<LoginPopup />} />
-                    <Route index element={<HomePage/>}/>
-                    <Route element={<Header/>}/>
-                    <Route  element={<LoginPopup />} />
-                    <Route path="/:pageId/:categoryId?" element={<DashboardTeacher />} /> 
-                    <Route path="/dashboard" element={<DashboardStudent />} /> 
-                    <Route path="/:pageId/:categoryId?"  element={<CourseSearch />} /> 
-                    {/* example <Route path="/:pageId"  element={<othercontent />} /> */}
-                </Route>
-            </Routes>
-        </BrowserRouter>
-        <Footer />
-=======
             <BrowserRouter>
                 <Routes>
-                    <Route index path='login' element={<LoginPopup />} />
+                    <Route path='login' element={<LoginPopup />} />
                     <Route path='/' element={<Header />}>
                         <Route path='home' element={<HomePage />} />
+                        <Route index element={<HomePage />} />
                         <Route path='auth' element={<Login />} />
                         <Route path="/:pageId/:categoryId?" element={<DashboardTeacher />} />
+                        <Route path="dashboard" element={<DashboardStudent />} />
                         <Route path="/courses/" element={<CourseSearch />} />
                         <Route path="/courses/:categoryId/:courseId" element={<CourseDetail />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
             <Footer />
->>>>>>> 71b3a9bf86b0fde194bc06bd8733a53c89036f79
         </>
     )
 }
