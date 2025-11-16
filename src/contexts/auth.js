51cb172e309d1154870f0d4879d2cff411f34a50
Matheus Paternez <matheusgfpaternez2006@gmail.com>
2025-11-16 12:36:56 -0800

    Add logic for auth localstorage

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,7 +1,23 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
-export const UserContext = createContext();
+export const AuthContext = createContext({});
 
-export function UserProvider({children}){
-    
-}
+export function AuthProvider({ children }) {
+
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        const userToken = localStorage.getItem("user_token");
+        const usersStorage = localStorage.getItem("users_db");
+
+        if (userToken && usersStorage) {
+            const hasUser = JSON.parse(usersStorage)?.filter(
+                (user) => user.email === JSON.parse(userToken).email
+            );
+
+            if (hasUser) setUser(hasUser[0]);
+        }
+    })
+
+    return <AuthContext.Provider>{children}</AuthContext.Provider>
+};
