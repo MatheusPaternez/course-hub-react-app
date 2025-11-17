85b29471088c123997973ea162d003267e9a8a04
Matheus Paternez <matheusgfpaternez2006@gmail.com>
2025-11-17 09:41:33 -0800

    Fix app.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CourseSearch from './pages/CourseSearch';
 import CourseDetail from './pages/CourseDetail';
-import Login from './components/Login';
 import DashboardTeacher from './pages/DashboardTeacher';
-<<<<<<< HEAD
 import { AuthProvider } from './contexts/auth';
 import useAuth from './hooks/useAuth';
+import CoursePageSection from './components/CoursePageSection';
+import DashboardStudent from './pages/DashboardStudent';
+import ContentManagement from './pages/ContentManagement';
 
 // Private route component using Auth context
 const Private = ({ Compo }) => {
@@ -17,49 +18,28 @@ const Private = ({ Compo }) => {
     // If signed is true, render the component, otherwise show login popup
     return signed ? <Compo /> : <LoginPopup />;
 }
-=======
-import CoursePageSection from './components/CoursePageSection';
-import DashboardStudent from './pages/DashboardStudent';
-import ContentManagement from './pages/ContentManagement';
->>>>>>> 4001ef06a5e1f7f1acf7e6b2b1ce47327e4d3177
 
 export default function App() {
     return (
         <>
-<<<<<<< HEAD
             <AuthProvider>
                 <BrowserRouter>
                     <Routes>
-                        <Route index path='login' element={<LoginPopup />} />
+                        <Route path='login' element={<LoginPopup />} />
                         <Route path='/' element={<Header />}>
-                            <Route path='home' element={<Private Compo={HomePage} />} />
-                            <Route path='auth' element={<LoginPopup />} />
+                            <Route path='home' element={<HomePage />} />
+                            <Route index element={<HomePage />} />
                             <Route path="/:pageId/:categoryId?" element={<DashboardTeacher />} />
-                            <Route path="/:pageId/:categoryId?" element={<CourseSearch />} />
+                            <Route path="dashboard" element={<DashboardStudent />} />
+                            <Route path="/courses/" element={<CourseSearch />} />
+                            <Route path="/courses/:categoryId/:courseId" element={<CourseDetail />} />
+                            <Route path='/courses/section/:courseId' element={<CoursePageSection />} />
+                            <Route path="/content" element={<ContentManagement />} />
                         </Route>
                     </Routes>
                 </BrowserRouter>
                 <Footer />
             </AuthProvider>
-=======
-            <BrowserRouter>
-                <Routes>
-                    <Route path='login' element={<LoginPopup />} />
-                    <Route path='/' element={<Header />}>
-                        <Route path='home' element={<HomePage />} />
-                        <Route index element={<HomePage />} />
-                        <Route path='auth' element={<Login />} />
-                        <Route path="/:pageId/:categoryId?" element={<DashboardTeacher />} />
-                        <Route path="dashboard" element={<DashboardStudent />} />
-                        <Route path="/courses/" element={<CourseSearch />} />
-                        <Route path="/courses/:categoryId/:courseId" element={<CourseDetail />} />
-                        <Route path='/courses/section/:courseId' element={<CoursePageSection />} />
-                        <Route path="/content" element={<ContentManagement />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-            <Footer />
->>>>>>> 4001ef06a5e1f7f1acf7e6b2b1ce47327e4d3177
         </>
     )
 }
